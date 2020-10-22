54ccb179a10f606da9be6bc3e73d4aea670d34ed
kkatzen <kristak@google.com>
2020-10-22 15:46:37 -0700

    Adding Vega Example

--- app/src/pages/ExploreDataPage.tsx
+++ app/src/pages/ExploreDataPage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import VegaTest from "../features/VegaTest";
 
 function ExploreDataPage() {
   return (
     <p>
       Research questions; explore key relationships across datasets, chosen by
       us; explore the data freely
+      <h1>Vega Example</h1>
+      <VegaTest />
     </p>
   );
 }
