f00fc5c0cc5f8c69594d50b53110528ad315517f
kkatzen <kristak@google.com>
2020-10-22 17:18:50 -0700

    Adding Observable notebook example

--- app/src/pages/ExploreDataPage.tsx
+++ app/src/pages/ExploreDataPage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ObservableTest from "../features/ObservableTest";
 import VegaTest from "../features/VegaTest";
 
 function ExploreDataPage() {
@@ -6,6 +7,8 @@ function ExploreDataPage() {
     <p>
       Research questions; explore key relationships across datasets, chosen by
       us; explore the data freely
+      <h1>Observable Notebook Example</h1>
+      <ObservableTest />
       <h1>Vega Example</h1>
       <VegaTest />
     </p>
