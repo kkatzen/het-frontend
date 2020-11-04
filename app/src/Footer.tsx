26bb0e0dc1864efbb11f14e7898848d1691159b4
kkatzen <kristak@google.com>
2020-11-04 16:35:07 -0500

    remove unused

--- app/src/Footer.tsx
+++ app/src/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Grid, { GridSpacing } from "@material-ui/core/Grid";
+import Grid from "@material-ui/core/Grid";
 import styles from "./Footer.module.scss";
 
 function Footer() {
