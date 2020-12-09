9adb729439310ed2f3b929d5272785ae5dda021b
kkatzen <kristak@google.com>
2020-12-09 11:29:16 -0500

    update prototype link

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -89,7 +89,7 @@ function AppToolbar() {
         )
       )}
       <Button className={styles.NavButton}>
-        <a href="https://satcherinstitute.github.io/data-visualization/02_covid19_death_disparities/">
+        <a href="https://zipline.appspot.com/view/zipline-prototypes/het-test-hawk/het-test---hawk8026/versions/1/data/index.html#/1">
           Prototypes
         </a>
       </Button>
