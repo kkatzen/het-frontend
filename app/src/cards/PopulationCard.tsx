5f7a4c8abe740dbe3de9e88f71c174add6355f0b
kkatzen <kristak@google.com>
2020-12-16 16:17:49 -0500

    cleanup

--- app/src/cards/PopulationCard.tsx
+++ app/src/cards/PopulationCard.tsx
@@ -34,63 +34,46 @@ function PopulationCard(props: { fips: Fips }) {
           ? totalPopulation["population"].toLocaleString("en")
           : "Data retrieval error";
 
-        console.log(dataset);
-
-        const populationMetrics = [
-          { label: "Median age", value: "38.2" },
-          { label: "White alone", value: "72%" },
-          { label: "Black or African American, alone", value: "12.8%" },
-          { label: "Asian alone", value: "5.7%" },
-          { label: "American Indian & Alaskan Native alone", value: "0.9%" },
-          { label: "Two or more races", value: "3.4%" },
-        ];
-
         return (
-          <>
+          <CardContent>
+            <span className={styles.PopulationCardTitle}>
+              {props.fips.getFullDisplayName()}
+            </span>
             {dataset.length < 1 && (
               <Alert severity="warning">
                 Missing data means that we don't know the full story.
               </Alert>
             )}
             {dataset.length > 0 && (
-              <>
-                <CardContent>
-                  <CardContent>
-                    <span className={styles.PopulationCardTitle}>
-                      {props.fips.getFullDisplayName()}
-                    </span>
-                  </CardContent>
-                  <Grid
-                    container
-                    className={styles.PopulationCard}
-                    justify="space-around"
-                  >
-                    <Grid item>
-                      <span>Total Population</span>
-                      <span className={styles.TotalPopulationValue}>
-                        {totalPopulationSize}
+              <Grid
+                container
+                className={styles.PopulationCard}
+                justify="space-around"
+              >
+                <Grid item>
+                  <span>Total Population</span>
+                  <span className={styles.TotalPopulationValue}>
+                    {totalPopulationSize}
+                  </span>
+                </Grid>
+                {/* TODO- calculate median age */}
+                <Grid item className={styles.PopulationMetric}>
+                  <span>Median Age</span>
+                  <span className={styles.PopulationMetricValue}>??</span>
+                </Grid>
+                {dataset
+                  .filter((r) => r.race_and_ethnicity !== "Total")
+                  .map((row) => (
+                    <Grid item className={styles.PopulationMetric}>
+                      <span>{row.race_and_ethnicity}</span>
+                      <span className={styles.PopulationMetricValue}>
+                        {row.population_pct}%
                       </span>
                     </Grid>
-                    {/* TODO- calculate median age */}
-                    <Grid item className={styles.PopulationMetric}>
-                      <span>Median Age</span>
-                      <span className={styles.PopulationMetricValue}>??</span>
-                    </Grid>
-                    {dataset
-                      .filter((r) => r.race_and_ethnicity !== "Total")
-                      .map((row) => (
-                        <Grid item className={styles.PopulationMetric}>
-                          <span>{row.race_and_ethnicity}</span>
-                          <span className={styles.PopulationMetricValue}>
-                            {row.population_pct}%
-                          </span>
-                        </Grid>
-                      ))}
-                  </Grid>
-                </CardContent>
-              </>
+                  ))}
+              </Grid>
             )}
-          </>
+          </CardContent>
         );
       }}
     </CardWrapper>
