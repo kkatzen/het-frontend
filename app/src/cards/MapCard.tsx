ad4aa17f58ca11e678dfa15c6a2514c7c432f35c
kkatzen <kristak@google.com>
2020-12-16 14:31:25 -0500

    remove unused

--- app/src/cards/MapCard.tsx
+++ app/src/cards/MapCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import UsaChloroplethMap from "../charts/UsaChloroplethMap";
 import { Fips } from "../utils/madlib/Fips";
 import Alert from "@material-ui/lab/Alert";
